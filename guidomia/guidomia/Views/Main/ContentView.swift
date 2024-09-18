b08826176386c22b2d7a048e4b506a983fee799d
Jade <jhade.lapuz@gmail.com>
2024-09-18 13:17:25 +0800

    [GENERAL] - organized folder structures and added local json file

---
+++ guidomia/guidomia/Views/Main/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guidomia
+//
+//  Created by Jade Lapuz on 9/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ guidomia/guidomia/Views/Main/GuidomiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidomiaApp.swift
+//  Guidomia
+//
+//  Created by Jade Lapuz on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GuidomiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
